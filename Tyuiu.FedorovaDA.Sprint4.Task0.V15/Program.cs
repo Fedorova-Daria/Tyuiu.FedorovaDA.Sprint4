64713041287d6e85079a87d78fd5d50a7b7649ec
Дарья Федорова <Tyuiu.FedorovaDA@outlook.com>
2024-10-06 13:03:06 +0500

    Add project files.

---
+++ Tyuiu.FedorovaDA.Sprint4.Task0.V15/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.FedorovaDA.Sprint4.Task0.V15.Lib;
+namespace Tyuiu.FedorovaDA.Sprint4.Task0.V15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #4 | Выполнила: Федрова Д. А. | АСОиУб-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Одномерные массивы (статический ввод)                             *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #15                                                             *");
+            Console.WriteLine("* Выполнила: Федорова Дарья Артемовна | АСОиУб-24-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан одномерный целочисленный массив на 10 элементов заполненный         *");
+            Console.WriteLine("* статическими значениями в диапазоне от 0 до 9                           *");
+            Console.WriteLine("* подсчитать произведение нечетных элементов массива.                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int[] numsArray = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            Console.WriteLine("Исходный массив:");
+            for (int i = 0; i < numsArray.Length; i++)
+            {
+                Console.WriteLine(numsArray[i]);
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            int res = ds.GetMultOddArrEl(numsArray);
+
+            Console.WriteLine("Произведение всех нечетных массивов равна:" + res);
+        }
+    }
+}
