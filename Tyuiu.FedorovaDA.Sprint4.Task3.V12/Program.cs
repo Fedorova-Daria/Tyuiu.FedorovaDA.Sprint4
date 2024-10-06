4dd4e2f180a3935f4e0defd35544c10d0b551ffe
Дарья Федорова <Tyuiu.FedorovaDA@outlook.com>
2024-10-06 14:57:30 +0500

    Task3

--- Tyuiu.FedorovaDA.Sprint4.Task3.V12/Program.cs
+++ Tyuiu.FedorovaDA.Sprint4.Task3.V12/Program.cs
@@ -1,55 +1,52 @@
-﻿using Tyuiu.FedorovaDA.Sprint4.Task2.V22.Lib;
-namespace Tyuiu.FedorovaDA.Sprint4.Task2.V22
+﻿using Tyuiu.FedorovaDA.Sprint4.Task3.V12.Lib;
+namespace Tyuiu.FedorovaDA.Sprint4.Task3.V12
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Random rnd = new Random();
-            DataService ds = new DataService();
 
+            int[,] array = new int[5, 5] { { 9, 7, 4, 9, 7 },
+                { 5, 2, 5, 7, 6 },
+                { 8, 3, 3, 5, 7 },
+                { 8, 4, 2, 4, 5 },
+                { 8, 8, 9, 5, 6 } };
+            int rows = array.GetUpperBound(0) + 1;
+            int colums = array.Length / rows;
+
+            DataService ds = new DataService();
             Console.Title = "Спринт #4 | Выполнила: Федрова Д. А. | АСОиУб-24-1";
 
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* Спринт #4                                                               *");
             Console.WriteLine("* Тема: Одномерные массивы. (ввод с клавиатуры)                           *");
-            Console.WriteLine("* Задание #2                                                              *");
-            Console.WriteLine("* Вариант #22                                                             *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #12                                                             *");
             Console.WriteLine("* Выполнила: Федорова Дарья Артемовна | АСОиУб-24-1                       *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* УСЛОВИЕ:                                                                *");
-            Console.WriteLine("* Дан одномерный целочисленный массив на 12 элементов заполненный         *");
-            Console.WriteLine("* значениями с клавиатуры в диапазоне от 2 до 9                           *");
-            Console.WriteLine("* подсчитать произведение нечетных элементов массива.                     *");
-            Console.WriteLine("* С клавиатуры: 8, 5, 4, 4, 3, 9, 9, 9, 3, 4, 4, 9                        *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов,                    *");
+            Console.WriteLine("* заполненный статическими значениями в диапазоне от 2 до 9.              *");
+            Console.WriteLine("* Найдите минимальный элемент третьем столбце массива.                    *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
             Console.WriteLine("***************************************************************************");
 
-            int len;
-            Console.WriteLine("Введите количество элементов массива:");
-            len = Convert.ToInt32(Console.ReadLine());
-
-            int[] numsArray = new int[len];
-
-            for (int i = 0; i <= len - 1; i++)
+            Console.WriteLine("Массив:");
+            for (int i = 0; i < rows; i++)
             {
-                numsArray[i] = rnd.Next(1,6);
+                for (int j = 0; j < colums; j++)
+                {
+                    Console.Write($"{array[i, j],4}");
+                }
+                Console.WriteLine();
             }
             Console.WriteLine();
-            Console.WriteLine("Массив:");
-            for (int i = 0; i <= len - 1; ++i)
-            {
-                Console.Write(numsArray[i] + "\t");
-            }
-
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
             Console.WriteLine("***************************************************************************");
-
-            int res = ds.Calculate(numsArray);
-            Console.WriteLine("Произведение нечетных элементов массива равна: " + res);
-            Console.ReadKey();
+            int res = ds.Calculate(array);
+            Console.WriteLine("Минимальный элемент массива в третьем столбце: " + res);
         }
     }
 }
